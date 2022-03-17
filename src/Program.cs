7ad87b5578da381da5e47c6538eb2572c16d6d08
JP Dillingham <jpdillingham@gmail.com>
2022-03-16 20:15:33 -0500

    add benchmark

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+var directory = @"\\wse\music\processed\Melvins";
+var files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
+Console.WriteLine($"Enumerated {files.Length} files.  Starting benchmark...");
+
+Benchmark("TagLib", () => ProcessWithTagLib(files));
+Benchmark("ATL", () => ProcessWithATL(files));
+
+void Benchmark(string name, Action action)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    action();
+    sw.Stop();
+    Console.WriteLine($"{name} completed in {sw.ElapsedMilliseconds}ms");
+}
+
+void ProcessWithTagLib(string[] files)
+{
+    foreach (var file in files)
+    {
+        var tfile = TagLib.File.Create(file);
+        //Console.WriteLine($"{file}: {tfile.Properties.Duration}, {tfile.Properties.AudioBitrate}");
+    }
+}
+
+void ProcessWithATL(string[] files)
+{
+    foreach (var file in files)
+    {
+        var theTrack = new ATL.Track(file);
+        //Console.WriteLine($"{file}: {theTrack.Duration}, {theTrack.Bitrate}");
+    }
+}
